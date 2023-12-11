c1bc4872fea721521d8ecc4e82e97e910eb81c24
Yuki <15008377+ykhirao@users.noreply.github.com>
2023-12-11 15:45:09 +0900

    add react

---
+++ jsx-without-react/react/src/App.js
@@ -0,0 +1,10 @@
+import { Button } from "./Button";
+
+const App = () => (
+  <div>
+    <h1>Hello world</h1>
+    <Button onClick={() => alert(1)}>Click 1</Button>
+  </div>
+);
+
+export default App;
---
+++ jsx-without-react/react/src/Button.js
@@ -0,0 +1,3 @@
+export const Button = ({ children, onClick }) => (
+  <button onClick={onClick}>{children}</button>
+);
